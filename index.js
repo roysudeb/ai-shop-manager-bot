11cf0d5f75b080cf66ea384b323110cb899b9ef9
roysudeb <120310080+roysudeb@users.noreply.github.com>
2026-06-18 12:23:35 +0530

    Update index.js

--- index.js
+++ index.js
@@ -585,7 +585,7 @@ async function handleMessage(chatId, text) {
 import express from 'express';
 
 const app  = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 app.use(express.json());
 
 // Telegram webhook endpoint
